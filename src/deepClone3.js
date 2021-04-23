65e785af34d4b45e9f49cd158309915ba5b070a2
EnochQin <qyqdream@gmail.com>
2021-04-23 17:41:57 +0800

    update: deepClone3: can clone more object
    can clone Array, Function, String, Boolean, Number, RegExp, Date, Error, Map, Set

--- src/deepClone3.js
+++ src/deepClone3.js
@@ -1,18 +1,50 @@
 const deepClone = (source) => {
   if (source instanceof Object) {
     let result;
-    if (source instanceof Array) {
-      result = new Array();
-    } else if (source instanceof Date) {
-      result = new Date();
-    } else if (source instanceof Function) {
-      result = function () {
-        return source.apply(this, arguments);
-      };
-    } else if (source instanceof RegExp) {
-      result = new RegExp(source.source, source.flags);
-    } else {
-      result = {};
+    switch (source.constructor.name) {
+      case "Object":
+        result = {};
+        break;
+      case "Array":
+        result = new Array();
+        break;
+      case "Date":
+        result = new Date();
+        break;
+      case "Function":
+        result = function () {
+          return source.apply(this, arguments);
+        };
+        break;
+      case "String":
+        result = new String(source.valueOf());
+        break;
+      case "Boolean":
+        result = new Boolean(source.valueOf());
+        break;
+      case "Number":
+        result = new Number(source.valueOf());
+        break;
+      case "Set":
+        result = new Set();
+        source.forEach((value) => {
+          result.add(deepClone(value));
+        });
+        break;
+      case "Map":
+        result = new Map();
+        for (let [key, value] of source) {
+          result.set(key, deepClone(value));
+        }
+        break;
+      case "Error":
+        result = new Error(source.message);
+        break;
+      case "RegExp":
+        result = new RegExp(source.source, source.flags);
+        break;
+      default:
+        result = {};
     }
     for (let key in source) {
       if (source.hasOwnProperty(key)) {
