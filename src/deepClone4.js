7fbecd7e97e2c3417fef53af2f850b680777f578
EnochQin <qyqdream@gmail.com>
2021-04-25 17:28:03 +0800

    refactor: deepClone4, 5: add cloneObject function

--- src/deepClone4.js
+++ src/deepClone4.js
@@ -1,29 +1,29 @@
 "use strict";
 const caches = [];
 const deepClone = (source) => {
-  if (source instanceof Object) {
-    const cachesResult = findCloneResultFromCaches(source);
-    if (cachesResult) return cachesResult;
-    const result = initResult(source);
-    caches.push([source, result]);
-    for (let key in source) {
-      if (source.hasOwnProperty(key)) {
-        const index = parseInt(key);
-        // String 对象用 for in 遍历，会自动遍历到 「第n个位置的字符」
-        // 而这个属性是只读属性，无法赋值，就会导致代码报错，需要跳过这些属性
-        if (
-          source.constructor.name !== "String" ||
-          (source.constructor.name === "String" &&
-            (index < 0 || index >= source.length || isNaN(index)))
-        ) {
-          result[key] = deepClone(source[key]);
-        }
+  return source instanceof Object ? cloneObject(source) : source;
+};
+
+const cloneObject = (source) => {
+  const cachesResult = findCloneResultFromCaches(source);
+  if (cachesResult) return cachesResult;
+  const result = initResult(source);
+  caches.push([source, result]);
+  for (let key in source) {
+    if (source.hasOwnProperty(key)) {
+      const index = parseInt(key);
+      // String 对象用 for in 遍历，会自动遍历到 「第n个位置的字符」
+      // 而这个属性是只读属性，无法赋值，就会导致代码报错，需要跳过这些属性
+      if (
+        source.constructor.name !== "String" ||
+        (source.constructor.name === "String" &&
+          (index < 0 || index >= source.length || isNaN(index)))
+      ) {
+        result[key] = deepClone(source[key]);
       }
     }
-    return result;
-  } else {
-    return source;
   }
+  return result;
 };
 
 const findCloneResultFromCaches = (source) => {
--- src/deepClone5.js
+++ src/deepClone5.js
@@ -4,29 +4,28 @@ class DeepClone {
     this.caches = [];
   }
   clone(source) {
-    if (source instanceof Object) {
-      const cachesResult = this.findCloneResultFromCaches(source);
-      if (cachesResult) return cachesResult;
-      const result = this.initResult(source);
-      this.caches.push([source, result]);
-      for (let key in source) {
-        if (source.hasOwnProperty(key)) {
-          const index = parseInt(key);
-          // String 对象用 for in 遍历，会自动遍历到 「第n个位置的字符」
-          // 而这个属性是只读属性，无法赋值，就会导致代码报错，需要跳过这些属性
-          if (
-            source.constructor.name !== "String" ||
-            (source.constructor.name === "String" &&
-              (index < 0 || index >= source.length || isNaN(index)))
-          ) {
-            result[key] = this.clone(source[key]);
-          }
+    return source instanceof Object ? this.cloneObject(source) : source;
+  }
+  cloneObject(source) {
+    const cachesResult = this.findCloneResultFromCaches(source);
+    if (cachesResult) return cachesResult;
+    const result = this.initResult(source);
+    this.caches.push([source, result]);
+    for (let key in source) {
+      if (source.hasOwnProperty(key)) {
+        const index = parseInt(key);
+        // String 对象用 for in 遍历，会自动遍历到 「第n个位置的字符」
+        // 而这个属性是只读属性，无法赋值，就会导致代码报错，需要跳过这些属性
+        if (
+          source.constructor.name !== "String" ||
+          (source.constructor.name === "String" &&
+            (index < 0 || index >= source.length || isNaN(index)))
+        ) {
+          result[key] = this.clone(source[key]);
         }
       }
-      return result;
-    } else {
-      return source;
     }
+    return result;
   }
   findCloneResultFromCaches(source) {
     for (let i = 0; i < this.caches.length; i++) {
